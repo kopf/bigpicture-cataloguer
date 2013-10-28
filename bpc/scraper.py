1ce7eb1800d13e1de2a78fa832d5ff5b3f56f6a6
Aengus Walton <ventolin@gmail.com>
2013-10-28 21:08:29 +0100

    Added list_album_photos functionality, preliminary text cleanup functions

--- bpc/scraper.py
+++ bpc/scraper.py
@@ -1,3 +1,5 @@
+import re
+
 from BeautifulSoup import BeautifulSoup
 
 import bpc.http as http
@@ -6,7 +8,10 @@ import bpc.http as http
 ALBUMS = 'http://www.boston.com/bigpicture/{year}/{{month:02d}}/'
 
 
-def get_albums(year, month):
+def list_albums(year, month):
+    """Returns a dictionary of the form {title: url} for
+    photo albums for a given month
+    """
     html = http.get(ALBUMS.format(year=year, month=month))
     soup = BeautifulSoup(html)
     divs = soup.findAll('div', {'class': 'headDiv2'})
@@ -20,4 +25,56 @@ def get_albums(year, month):
     else:
         for div in divs:
             retval[div.a.text] = div.a['href']
+    return retval
+
+
+def get_album_intro_text(soup):
+    """Returns an album's intro text
+    """
+    return clean_caption_text(soup.find('div', {'class': 'bpBody'}))
+
+
+def make_image_dict(div):
+    """Returns a dict in the form {'url': url, 'caption': '...'} for an
+    image div
+    """
+    return {
+        'url': div.find('img')['src'],
+        'caption': clean_caption_text(div.find('div', {'class': 'bpCaption'}))
+    }
+
+
+def clean_caption_text(div):
+    """Removes links from captions, "(x photos total)" messages, etc
+    """
+    segments = []
+    for content in div.contents:
+        if isinstance(content, basestring):
+            segments.append(content)
+        else:
+            segments.append(content.text)
+    retval = u''.join(segments)
+    retval = re.sub(r'\(\d\d photos total\)', '', retval)
+    return retval.strip()
+
+
+def list_album_photos(url):
+    """Returns a list of dictionaries representing images in
+    the form [{'url': url, 'caption': caption}, ...]
+    """
+    html = http.get(url)
+    soup = BeautifulSoup(html)
+
+    # Process top image, prepending album intro text to caption
+    top_image_div = soup.find('div', {'class': 'bpImageTop'})
+    entry = make_image_dict(top_image_div)
+    intro_text = get_album_intro_text(soup)
+    entry['caption'] = u'{0} || {1}'.format(
+        intro_text, entry['caption'])
+    retval = [entry]
+
+    # Process rest of images
+    divs = soup.findAll('div', {'class': 'bpBoth'})
+    for div in divs:
+        retval.append(make_image_dict(div))
     return retval
--- bpc/tests/test_scraper.py
+++ bpc/tests/test_scraper.py
@@ -2,14 +2,14 @@ import unittest
 
 from mock import patch
 
-from bpc.scraper import get_albums
+from bpc.scraper import list_albums, list_album_photos
 import bpc.http
 from bpc.tests.tests import FIXTURES
 
 
 class TestScraper(unittest.TestCase):
     @patch.object(bpc.http, 'get', return_value=FIXTURES['2008-05.html'])
-    def test_get_albums_old_site(self, mocked_get):
+    def test_list_albums_old_site(self, mocked_get):
         """Should return a dictionary when parsing old (before sep 2008) months"""
         expected = {
             'Cassini Nears Four-year Mark': 
@@ -17,10 +17,10 @@ class TestScraper(unittest.TestCase):
             'Uncontacted Tribe Photographed in Brazil': 
                 'http://www.boston.com/bigpicture/2008/05/uncontacted_tribe_photographed.html'
         }
-        self.assertEqual(expected, get_albums(2008, 8))
+        self.assertEqual(expected, list_albums(2008, 8))
 
     @patch.object(bpc.http, 'get', return_value=FIXTURES['2008-09.html'])
-    def test_get_albums_new_site(self, mocked_get):
+    def test_list_albums_new_site(self, mocked_get):
         """Should return a dictionary when parsing newer (after sep 2008) months"""
         expected = {
             'The Singapore Grand Prix':
@@ -28,4 +28,23 @@ class TestScraper(unittest.TestCase):
             'Childhood Cancer Awareness Month':
                 'http://www.boston.com/bigpicture/2008/09/childhood_cancer_awareness_mon.html'
         }
-        self.assertEqual(expected, get_albums(2008, 9))
+        self.assertEqual(expected, list_albums(2008, 9))
+
+    @patch.object(bpc.http, 'get',
+        return_value=FIXTURES['cassini_nears_fouryear_mark.html'])
+    def test_list_album_photos(self, mocked_get):
+        """Should return a list of photos when given a photo album url"""
+        expected = [
+            {'caption': (u"NASA's Cassini Spacecraft is now reaching the end of its four-year prime mission "
+                          "(on June 30th), and about to enter into its extended mission. What a nice excuse "
+                          "for a retrospective of some of the great images sent back home by Cassini over "
+                          "the past four years. || The Sun is on the opposite side, so all of Saturn is "
+                          "backlit. Courtesy NASA/JPL-Caltech"),
+             'url': u'http://cache.boston.com/universal/site_graphics/blogs/bigpicture/saturn_05_30/cassini1.jpg'},
+            {'caption': u"Swirls in Saturn's cloud-tops. Courtesy NASA/JPL-Caltech",
+             'url': u'http://cache.boston.com/universal/site_graphics/blogs/bigpicture/saturn_05_30/cassini2.jpg'},
+            {'caption': u"The surface of Saturn's moon Dione, up close. Courtesy NASA/JPL-Caltech",
+             'url': u'http://cache.boston.com/universal/site_graphics/blogs/bigpicture/saturn_05_30/cassini3.jpg'},
+        ]
+        print list_album_photos('bla')
+        self.assertEqual(expected, list_album_photos('bla'))
