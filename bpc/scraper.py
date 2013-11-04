c5982a352696d9cbdfb97849b5d7ca0fa3e38b69
Aengus Walton <ventolin@gmail.com>
2013-11-04 21:41:07 +0100

    fix apostrophe bug properly

--- bpc/scraper.py
+++ bpc/scraper.py
@@ -31,7 +31,7 @@ def list_albums(year, month):
     for album in retval:
         for char in invalid_chars:
             album['name'] = album['name'].replace(char, '_').strip('.')
-            album['name'] = album['name'].replace(u"’", "'")
+            album['name'] = album['name'].replace(u"\x92", "'")
     return retval[::-1]
 
 
