1934d79489bc6d1f7b5ea4c1b3ffd4e2d52e8305
Aengus Walton <ventolin@gmail.com>
2013-11-09 12:57:00 +0100

    Compile bpc into one single exe file

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
+
 import py2exe
 
-setup(console=['bpc/bigpicture.py'])
+
+py2exe_options = {
+    'py2exe': {
+        'bundle_files': 1,
+        'compressed': True,
+        'dll_excludes': 'w9xpopen.exe'
+    }
+}
+
+
+setup(console=['bpc/bigpicture.py'], zipfile=None,
+      options=py2exe_options)
